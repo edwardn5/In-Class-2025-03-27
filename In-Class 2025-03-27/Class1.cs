d56db54ce6ec3bd5822d446ef61e45a7ffe045de
Jenna Liette <lietteja@mail.uc.edu>
2025-03-27 11:46:04 -0400

    Add project files.

---
+++ In-Class 2025-03-27/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace In_Class_2025_03_27
+{
+    internal class Shoe
+    {
+        private double _price;
+
+        // Create getter and a setter for the private properties
+        public double Price { get { return _price; } set { _price = value; } }
+    }
+}
---
+++ In-Class 2025-03-27/Program.cs
@@ -0,0 +1,23 @@
+﻿//Jenna Liette
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace In_Class_2025_03_27
+{
+    /// <summary>
+    /// In class assignment showing classes, properties, and methods for a project. Incorporating GitHub forking and cloning. Modeling pricing for shoes. 
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shoe myShoe = new Shoe(); // Declare and instantiate
+            myShoe.Price = 49.99;  // Invoke the setter
+            Console.WriteLine("Price of my new pair of shoes is " + myShoe.Price); // getter
+        }
+    }
+}
